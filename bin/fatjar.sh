92acec1a00046fbcaca43b0fdcc942ea185e7046
sujitpal <sujit.pal@comcast.net>
2012-08-31 12:43:53 -0700

    Copying scala-libs.jar into $HADOOP_HOME/lib eliminates the need to repackage it into the fat jar.

--- bin/fatjar.sh
+++ bin/fatjar.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-SCALA_HOME=/opt/scala-2.9.2
 MAHOUT_HOME=/opt/mahout-distribution-0.7
 
 mkdir -p target/fatjar
 cd target/fatjar
 jar xvf $MAHOUT_HOME/core/target/mahout-core-0.7-job.jar
-jar xvf $SCALA_HOME/lib/scala-library.jar
 jar xvf ../scala-2.9.2/mia-scala-examples_2.9.2-1.0.jar
 jar cvf /tmp/my-mahout-fatjar.jar *
 cd -
